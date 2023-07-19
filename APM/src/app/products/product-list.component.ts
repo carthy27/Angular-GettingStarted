0e4674ac5a056daed82984e33c8e579e55b61e4a
Thomas McCarthy <Thomas.McCarthy@amcsgroup.com>
2023-07-19 14:57:29 +0100

    Navigation and Routing basics.

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -4,7 +4,6 @@ import { ProductService } from "./product.service";
 import { Subscription } from "rxjs";
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
@@ -56,7 +55,7 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnDestroy(){
-      this.sub.unsubscribe
+      this.sub.unsubscribe 
     }
 
     onRatingClicked(message: string): void{
